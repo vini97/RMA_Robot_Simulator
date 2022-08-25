448aa5959ba45270ed26c2147d90d75c26ce4aa8
vinicius.viana <vinicius_vsouza@hotmail.com>
2022-08-25 08:58:59 -0300

    Explorando Marte - v1

---
+++ Assets/Resources/Scripts/RobotScript.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotScript : MonoBehaviour{
+
+    CharacterController controller;
+    
+    Vector3 forward;
+    Vector3 strafe;
+    Vector3 vertical;
+
+    float forwardSpeed = 5f;
+    float strafeSpeed = 5f;
+
+    float gravity;
+    float jumpSpeed;
+    float maxJumpHeight = 2f;
+    float timeToMaxHeight = 0.5f;
+
+    void Start(){
+
+        controller = GetComponent<CharacterController>();
+
+        gravity = (-2 * maxJumpHeight) / (timeToMaxHeight * timeToMaxHeight);
+        jumpSpeed = (2 * maxJumpHeight) / timeToMaxHeight;
+        
+    }
+
+    void Update(){
+
+        float forwardInput = Input.GetAxisRaw("Vertical");
+        float strafeInput = Input.GetAxisRaw("Horizontal");
+
+        forward = forwardInput * forwardSpeed * transform.forward;
+        strafe = strafeInput * strafeSpeed * transform.right;
+
+        vertical += gravity * Time.deltaTime * Vector3.up;
+
+        if(controller.isGrounded){
+            vertical = Vector3.down;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space) && controller.isGrounded){
+            vertical = jumpSpeed * Vector3.up;
+        }
+
+        Vector3 finalVelocity = forward + strafe + vertical;
+
+        controller.Move(finalVelocity * Time.deltaTime);
+        
+    }
+}
